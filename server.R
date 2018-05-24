339fca1390e65b4e216b5497e214a165de7c4d50
unknown <akoozer1@gmail.com>
2018-05-24 11:31:37 -0700

    Added file

--- server.R
+++ server.R
@@ -4,6 +4,9 @@ library(dplyr)
 library(plotly)
 library(ggplot2)
 
+#Read in data in csv file
+data <- read.csv("data/LA-crimes.csv", stringsAsFactors = FALSE)
+
 # Start shiny server
 shinyServer(function(input, output) {
 
