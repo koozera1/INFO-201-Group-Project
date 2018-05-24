75a9a0cd3b4988d4b3ecdee6482720e255ed8604
unknown <akoozer1@gmail.com>
2018-05-24 13:14:14 -0700

    test

--- server.R
+++ server.R
@@ -6,8 +6,24 @@ library(ggplot2)
 
 #Read in data in csv file
 data <- read.csv("data/LA-crimes.csv", stringsAsFactors = FALSE)
+vari <- strsplit(data$Location, ",")
+
+test %>% 
+  
+
+data <- data %>% 
+  mutate(lat = lapply(substr(vari, 2, nchar(vari)))) %>% 
+  mutate(lng = substr(data$Location[2], 1, nchar(data$Location) - 2))
+
 
 # Start shiny server
 shinyServer(function(input, output) {
 
 })
+
+
+test <- strsplit(data$Location, ",")
+test[[1]][1]
+
+
+lapply()
